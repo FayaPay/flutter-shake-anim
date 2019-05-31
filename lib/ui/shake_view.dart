8d16e85df049295ccb40947c3be5ad3b55c20d01
Precious Keyz <ositachima.co@gmail.com>
2019-05-31 07:17:35 +0000

    refactor 1

--- lib/ui/shake_view.dart
+++ lib/ui/shake_view.dart
@@ -4,17 +4,15 @@ import 'package:flutter/material.dart';
 
 class ShakeView extends StatelessWidget {
   final Widget child;
-  final ShakeViewController controller;
-  final Animation _first_anim, _second_anim;
+  final ShakeController controller;
+  final Animation _anim;
+  final int tiltAngle;
 
-  ShakeView({@required this.child, @required this.controller})
-      : _first_anim = IntTween(begin: 0, end: 10).animate(CurvedAnimation(
+  ShakeView(
+      {@required this.child, @required this.controller, this.tiltAngle = 20})
+      : _anim = IntTween(begin: 0, end: tiltAngle).animate(CurvedAnimation(
           parent: controller,
-          curve: Interval(0.0, 0.5, curve: Curves.easeIn),
-        )),
-        _second_anim = IntTween(begin: 10, end: -10).animate(CurvedAnimation(
-          parent: controller,
-          curve: Interval(0.5, 1.0, curve: Curves.easeOut),
+          curve: Curves.bounceIn,
         ));
 
   @override
@@ -24,14 +22,14 @@ class ShakeView extends StatelessWidget {
       child: child,
       builder: (context, child) => Transform.rotate(
           child: child,
-          origin: Offset(0.0, 0.0),
-          angle: (maths.pi / 180) * _first_anim.value),
+          origin: Offset(10.0, 10.0),
+          angle: (maths.pi / 180) * _anim.value),
     );
   }
 }
 
-class ShakeViewController extends AnimationController {
-  ShakeViewController({@required TickerProvider vsync, Duration duration})
+class ShakeController extends AnimationController {
+  ShakeController({@required TickerProvider vsync, Duration duration})
       : super(vsync: vsync, duration: duration);
 
   shake() async {
